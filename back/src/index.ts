435aaf0098835da88a718f920fac15728a747a03
inesppino <inesp.pino@gmail.com>
2024-05-18 13:55:05 +0200

    :sparkles: add cors to handle cors error

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { getProperties } from "./controllers/propertyController";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.get("/api/properties", getProperties);
 
