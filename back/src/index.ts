6c85ec90748bfea415705d5dea5c931f5920a61a
inesppino <inesp.pino@gmail.com>
2024-05-18 13:40:45 +0200

    :sparkles: creates api entry in server

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import { getProperties } from "./controllers/propertyController";
 
 const app = express();
 const PORT = 3000;
+
+app.get("/api/properties", getProperties);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
